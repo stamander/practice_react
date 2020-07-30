f189139f4e127f9fc86a4f9add85e2f2b51bd5a9
stamander <iida.atsushi.fh@gmail.com>
2020-07-30 20:44:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {name:"にんじゃわんこ"}
+
+
+    
+  }
   render() {
     const text = "Hello World";
+    console.log(this.state);
     return (
       <div>
         <h1>こんにちは{text}さん</h1>
