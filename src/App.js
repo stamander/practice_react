665a29f8d4e96236ed19ea2a1a94d99490d7f59a
stamander <iida.atsushi.fh@gmail.com>
2020-07-30 20:24:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <img src = 'https://prog-8.com/family.jpg'/>
+        <img src = 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ninjawanko.png'/>
 
       </div>
     );
+  }
+}
+
+export default App;
+
+
+import React from 'react';
+class App extends React.Component{
+  render(){
+
   }
 }
 
