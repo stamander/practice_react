3f673d3a6f05eae6eba12779d62fb3033edee065
stamander <iida.atsushi.fh@gmail.com>
2020-07-30 20:27:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
+    const text = "Hello World";
     return (
       <div>
+        {text}
         <img src = 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ninjawanko.png'/>
 
       </div>
