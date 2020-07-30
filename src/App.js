14073858113dd9b8b391b3c7c762ce373a1f7351
stamander <iida.atsushi.fh@gmail.com>
2020-07-30 20:10:36 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello React</h1>
+      <div>
+        <h1>h1です</h1>
+        <h2>h2です</h2>
+        <p>pです</p>
+      </div>
     );
   }
 }
