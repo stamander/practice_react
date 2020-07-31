599f27e01bb5d2bf7f5e250ede35f0166546ab0d
stamander <iida.atsushi.fh@gmail.com>
2020-07-31 20:25:06 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,33 +5,39 @@ class App extends React.Component {
 
   constructor(props){
     super(props);
-   
+    this.state = {name: "にんじゃわんこ"};
     this.state = {count:0};
   }
 
-  handleClick(name){
+  hundleClick(name){
     this.setState({name:name});
     
   }
 
   handleClick(){
     this.setState({count:this.state.count +1 });
+    
+  }
+
+  mainasClick(){
+    this.setState({count:this.state.count -1 });
   }
   render() {
     const text = "Hello World";
     console.log(this.state);
     return (
       <div>
+        <h1>{text}</h1>
         <h1>こんにちは{this.state.name}さん</h1>
-        <button onClick = {()=> {this.handleClick("にんじゃわんこ")}}>
+        <button onClick = {()=> {this.hundleClick("にんじゃわんこ")}}>
           にんじゃわんこ
           </button>
-        <button onClick = {() => {this.handleClick("ひつじ仙人")}}>ひつじ仙人</button>
+        <button onClick = {() => {this.hundleClick("ひつじ仙人")}}>ひつじ仙人</button>
 
         <h1>{this.state.count}</h1>
         <button onClick = {() => {this.handleClick()}}>+</button>
         
-        <button>-</button>
+        <button onClick = {() => {this.mainasClick()}}>-</button>
 
 
       </div>
