3287e9d38a10efa7dc36b8325848828e29d15d2c
stamander <iida.atsushi.fh@gmail.com>
2020-07-31 16:38:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+
+
   constructor(props){
     super(props);
-    this.state = {name:"にんじゃわんこ"}
+   
+    this.state = {count:0};
+  }
 
-
+  handleClick(name){
+    this.setState({name:name});
     
+  }
+
+  handleClick(){
+    this.setState({count:this.state.count +1 });
   }
   render() {
     const text = "Hello World";
@@ -14,8 +23,15 @@ class App extends React.Component {
     return (
       <div>
         <h1>こんにちは{this.state.name}さん</h1>
-        <button onClick = {()=> {this.setState({name:"にんじゃわんこ"})}}>にんじゃわんこ</button>
-        <button onClick = {() => {this.setState({name:"ひつじ仙人"})}}>ひつじ仙人</button>
+        <button onClick = {()=> {this.handleClick("にんじゃわんこ")}}>
+          にんじゃわんこ
+          </button>
+        <button onClick = {() => {this.handleClick("ひつじ仙人")}}>ひつじ仙人</button>
+
+        <h1>{this.state.count}</h1>
+        <button onClick = {() => {this.handleClick()}}>+</button>
+        
+        <button>-</button>
 
 
       </div>
