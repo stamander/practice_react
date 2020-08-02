69044d48be9d7d8f4bcb5b34d5aa73895fc0a863
stamander <iida.atsushi.fh@gmail.com>
2020-08-02 10:49:21 +0900

    Create Lesson.js

---
+++ src/Lesson.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class Lesson  extends React.Component{
+
+  render(){
+    return(
+          <div className='lesson-card'>
+            <div className='lesson-item'>
+              <p></p>
+              <img />
+            </div>
+          </div>
+
+    );
+  }
+
+
+}
+
+
+
+
+
+
+export default Lesson;
