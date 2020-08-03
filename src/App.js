22fa45724ce0734e8fe8e9fdb2f99b6a9283dba6
stamander <iida.atsushi.fh@gmail.com>
2020-08-03 10:23:52 +0900

    modal

--- src/App.js
+++ src/App.js
@@ -1,113 +1,18 @@
 import React from 'react';
-import Language from './Language';
-import Lesson from './Lesson';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 class App extends React.Component {
-
-
-  constructor(props){
-    super(props);
-    this.state = {name: "にんじゃわんこ"};
-    this.state = {count:0};
-  }
-
-  hundleClick(name){
-    this.setState({name:name});
-    
-  }
-
-  handleClick(){
-    this.setState({count:this.state.count +1 });
-    
-  }
-
-  mainasClick(){
-    this.setState({count:this.state.count -1 });
-  }
   render() {
-    const text = "Hello World";
-    console.log(this.state);
-    
-    const Languagelist = [
-      {
-        name : " HTML & css ", 
-        image :"https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg",
-        introduction : "webページを構成する要素"
-      },
-
-      {
-        name : "Javascript",
-        image : "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg",
-        introduction : "webページを構成する要素"
-      },
-
-      {
-        name : "React",
-        image : "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg",
-        introduction : "webページを構成する要素"
-
-      }
-
-      
-    ]
-
-
-
-
-    const lessonItem = [
-      {
-        name : "HTML&CSS",
-        image : "https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg",
-        introduction : "webページを構成する要素"
-      }
-    ]
-
-
-
     return (
       <div>
-        <h1 className = "title">{text}</h1>
-        <h1 >こんにちは{this.state.name}さん</h1>
-        <button onClick = {()=> {this.hundleClick("にんじゃわんこ")}}>
-          にんじゃわんこ
-          </button>
-        <button onClick = {() => {this.hundleClick("ひつじ仙人")}}>ひつじ仙人</button>
-
-        <h1>{this.state.count}</h1>
-        <button onClick = {() => {this.handleClick()}}>+</button>
-        
-        <button onClick = {() => {this.mainasClick()}}>-</button>
-
-        <Lesson
-              name = {lessonItem.name}
-              img = {lessonItem.image}
-              />
-
-        <div className = "language">
-
-
-
-          {Languagelist.map((LanguageItem)=>{
-            
-            return(
-              <Language
-
-              name = {LanguageItem.name}
-              image = {LanguageItem.image}
-              introduction ={LanguageItem.introduction}
-              
-              />
-
-              );
-            })}
-            
-
-
-        </div>
+        <Header />
+        <Main />
+        <Footer />
       </div>
     );
   }
 }
 
 export default App;
-
--- src/Lesson.js
+++ src/Lesson.js
@@ -1,31 +1,51 @@
 import React from 'react';
 
 class Lesson extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isModalOpen: false};
+  }
+
+  handleClickLesson() {
+    this.setState({isModalOpen: true});
+  }
+
+  handleClickClose() {
+    this.setState({isModalOpen: false});
+  }
+
   render() {
-    return (
-      <div className='lesson-card'>
-        <div className='lesson-item'>
-          <p>{this.props.name}</p>
-          <img src={this.props.image} />
-        </div>
-        {/* モーダルを用意してください */}
+    let modal;
+    if (this.state.isModalOpen) {
+      modal = (
         <div className='modal'>
           <div className='modal-inner'>
             <div className='modal-header'></div>
             <div className='modal-introduction'>
-              {/* レッスンの名前を表示してください */}
               <h2>{this.props.name}</h2>
-              
-              {/* レッスンの紹介文を表示してください */}
               <p>{this.props.introduction}</p>
-              
             </div>
-            <button className='modal-close-btn'>
+            <button
+              className='modal-close-btn'
+              onClick={() => this.handleClickClose()}
+            >
               とじる
             </button>
           </div>
         </div>
-        
+      );
+    }
+
+    return (
+      <div className='lesson-card'>
+        <div
+          className='lesson-item'
+          onClick={() => {this.handleClickLesson()}}
+        >
+          <p>{this.props.name}</p>
+          <img src={this.props.image} />
+        </div>
+        {modal}
       </div>
     );
   }
