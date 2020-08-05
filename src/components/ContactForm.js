6aa495cfcc298c14c322108beb511044563c7539
stamander <iida.atsushi.fh@gmail.com>
2020-08-05 19:50:33 +0900

    お問い合わせフォーム

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,7 +8,9 @@ class ContactForm extends React.Component {
 
     this.state = {
       isSubmitted:false,
-      email:""
+      email:"",
+      hasEmailError:false,
+      content:"",
     }
 
 
@@ -19,8 +21,48 @@ class ContactForm extends React.Component {
     this.setState({isSubmitted:true});
   }
 
-  handle
+  handleEmailChange(event){
+    const inputValue = event.target.value
+    const isEmpty = inputValue ===""
+    this.setState(
+      {
+        email:inputValue,
+        hasEmailError:isEmpty,
+    
+    })
+  }
+
+  handleContentCharge(event){
+
+    const inputValue = event.target.value
+    const isEmpty = inputValue === ""
+    this.setState({
+
+      content:inputValue,
+      hasContentError:isEmpty
+    })
+
+  }
   render() {
+    let EmailErrorText;
+    if(this.state.hasEmailError){
+      EmailErrorText = (
+        <p className = "contact-message-error">
+          Emailを入力してください
+        </p>
+      )
+    }
+
+    let contentErrorText;
+    if(this.state.hasContentError){
+      contentErrorText = (
+        <p className = "contact-message-error">
+        お問い合わせ内容を入力してください
+      </p>
+
+      );
+    }
+
     let ContactForm;
     if(this.state.isSubmitted){
       ContactForm = (
@@ -28,7 +70,6 @@ class ContactForm extends React.Component {
           送信完了
           </div>
       )
-
     }else{
       ContactForm = (
         <form onSubmit = {() => {this.handleSubmit()}}>
@@ -36,12 +77,17 @@ class ContactForm extends React.Component {
         {/* inputタグを追加してください */}
         <input
           value = {this.state.email}
-          onChange = {(event)=>{console.log(event.target.value)}}
+          onChange = {(event)=>{this.handleEmailChange(event)}}
         />
-        
+        {EmailErrorText}
         <p>お問い合わせ内容（必須）</p>
         {/* textareaタグを追加してください */}
-        <textarea/>
+        <textarea 
+          value = {this.state.content}
+          onChange = {(event)=>{this.handleContentCharge(event)}}
+          
+          />
+          {contentErrorText}
         
         {/* 送信ボタンを追加してください */}
         <input 
@@ -50,8 +96,13 @@ class ContactForm extends React.Component {
         />
         
       </form>
-
       )
+
+
+        
+
+
+      
 
     }
 
