42a252e3ae82ccc678dc3caf6f68adec981c7439
stamander <iida.atsushi.fh@gmail.com>
2020-08-13 22:03:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello React</h1>
+        <h1>Hello World</h1>
         <img src ='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ninjawanko.png'/>
 
       </div>
